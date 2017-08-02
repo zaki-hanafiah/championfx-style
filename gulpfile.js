95c7009b13bc5abd9c6510835976703eb91c2baf
kelly <kelly@binary.com>
2017-08-02 15:00:38 +0800

    Update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,5 @@
 var gulp          = require('gulp');
-var deploy        = require('gulp-gh-pages');
-var sass          = require('gulp-sass');
-var cleanCSS      = require('gulp-clean-css');
 var gulpStylelint = require('gulp-stylelint');
-
-/**
- * Push build to gh-pages
- */
-// gulp.task('deploy', function () {
-//   return gulp.src("./dist/**/*")
-//     .pipe(deploy())
-// });
-
-/**
- * Compile sass to css
- */
-gulp.task('sass', function () {
-  return gulp.src('./_sass/all.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./dist/css'));
-});
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
 
 /**
  * Lint css
